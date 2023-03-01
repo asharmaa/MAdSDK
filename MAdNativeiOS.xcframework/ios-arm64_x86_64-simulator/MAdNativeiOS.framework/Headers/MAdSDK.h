33d36be8625723da99e788ab4bb6f7a07499826b
asharma <asharma@appnexus.com>
2023-03-01 22:21:29 +0530

    first commit

---
+++ MAdNativeiOS.xcframework/ios-arm64_x86_64-simulator/MAdNativeiOS.framework/Headers/MAdSDK.h
@@ -0,0 +1,26 @@
+/*   (C) 2022 Microsoft Group of Companies
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MAdiOS.
+FOUNDATION_EXPORT double MAdiOSVersionNumber;
+
+//! Project version string for MAdSDK.
+FOUNDATION_EXPORT const unsigned char MAdiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MAdiOS/PublicHeader.h>
+
+
